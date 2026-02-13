eba57dd6c35b2cd438612f90defa23fd014067f5
Laxmi <laxmithakuri640@gmail.com>
2026-02-13 14:17:58 +0545

    express.complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Hello world")
+
+const express= require("express");
+const app = express();
+
+const PORT= 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+});
+
+app.get("/", (req, res) =>{
+    res.send("laxmi thakuri")
+});
